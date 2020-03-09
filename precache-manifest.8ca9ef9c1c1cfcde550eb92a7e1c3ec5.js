2f890deeee892cd97f5ddfea41bdfff8a294a12e
Kushich <kushich@outlook.com>
2020-03-09 19:13:36 +0300

    Updates

---
+++ precache-manifest.8ca9ef9c1c1cfcde550eb92a7e1c3ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad9c93b4d1892e2be5dab03cb3f202d",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "3df34e768264f3631a74",
+    "url": "/note-app/static/css/main.f54fad84.chunk.css"
+  },
+  {
+    "revision": "0376d16e1ce64142d016",
+    "url": "/note-app/static/js/2.44fd3f06.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/note-app/static/js/2.44fd3f06.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3df34e768264f3631a74",
+    "url": "/note-app/static/js/main.46bc2040.chunk.js"
+  },
+  {
+    "revision": "9f52c3a9ac4307bd1972",
+    "url": "/note-app/static/js/runtime-main.6628c0d1.js"
+  }
+]);
